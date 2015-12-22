71a06d98ecb427dff8668ed106720471c8e960d1
Phil Chapman <phil.chapman@cruk.manchester.ac.uk>
2015-12-22 16:23:04 +0000

    Version 0.1.3: Added univariateVolcanoPlot function
    Visualisaiton of univariate analysis results

---
+++ R/univariateVolcanoPlot.R
@@ -0,0 +1,61 @@
+#' univariateVolcanoPlot
+#' Makes a volcano plot from the output of the univariateAnalysis function
+#' @param df Output data from from CancerCellLinesModelling::univariateAnalysis
+#' @param pval_th p-value threshold (unlogged)
+#' @param effect_th effect threshold (unlogged - ie 2 = 2 fold up or down)
+#' @return plot
+#' @export
+#' @import dplyr ggplot2 scales
+
+univariateVolcanoPlot <- function (ua_df, pval_th=NULL, effect_th=2, use_fdr=FALSE) {
+
+    #put effect_th on log scale
+    effect_th <- log10(effect_th)
+
+    #suitable pval_th
+    pval_th <- ifelse(use_fdr, 0.2, 0.05)
+
+    #define required pvalue
+    if(use_fdr) {
+        volcano_data <- ua_df %>% dplyr::mutate(pval_selected = FDR)
+    } else {
+        volcano_data <- ua_df %>% dplyr::mutate(pval_selected = p.value)
+    }
+
+    #define which genetic features exceed the thresholds set
+    volcano_data <- volcano_data %>%
+        dplyr::filter(!is.na(FDR)) %>%
+        dplyr::mutate(class = ifelse(estimate < -effect_th & pval_selected < pval_th, 'neg',
+                              ifelse(estimate > effect_th & pval_selected < pval_th, 'pos', 'same')))
+
+
+
+    #make a seperate data frame for the labels
+    volcano_data_geomtext <- volcano_data %>% filter(class != 'same')
+
+    #define a local function to plot the y axis as we want it
+    reverselog_trans <- function(base = exp(1)) {
+        trans <- function(x) -log(x, base)
+        inv <- function(x) base^(-x)
+        trans_new(paste0("reverselog-", format(base)), trans, inv,
+                  log_breaks(base = base),
+                  domain = c(1e-100, Inf))
+    }
+
+    #do the volcano plot
+    volcano_plot <- ggplot ( volcano_data , aes (x=effect_size_unlog, y=pval_selected, fill=class, size=log10(gt1))) + geom_point(aes(shape=feature_type)) +
+        geom_text(data = volcano_data_geomtext, aes(label=ID, colour=class), size=4, hjust=-0.3, vjust=-0.4, angle=0) +
+        geom_vline(xintercept = c(10^(-effect_th),10^effect_th) , color='blue', alpha=0.5, linetype='dotted') +
+        geom_hline(yintercept = pval_th, color='blue', alpha=0.5, linetype='dotted') +
+        scale_y_continuous(trans=reverselog_trans(10)) + scale_fill_manual (values=c('lightgreen', 'orange', 'lightgray')) +
+        scale_colour_manual (values = c('darkgreen', 'red', 'blue')) + scale_shape_manual(values = c(21,22,23), breaks = c("hybcap", "cosmic", "custom")) +
+        scale_size_continuous(range=c(3,13)) + scale_x_log10(limits=c(min(volcano_data$effect_size_unlog,0.1),max(10,volcano_data$effect_size_unlog))) +
+        xlab("IC50 Ratio") + ylab(ifelse(use_fdr, 'FDR', 'P value')) +
+        facet_wrap( ~ resp_id) +
+        theme_bw()  + theme(legend.position="none", axis.text = element_text(size=rel(1.5)), axis.title = element_text(size=rel(2)))
+
+    return(volcano_plot)
+
+
+
+}
